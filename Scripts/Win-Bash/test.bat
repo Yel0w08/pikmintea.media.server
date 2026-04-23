125412aa936cbaa01bfaf896f51642573c190adc
Yel0wPikmin <nathanael.mone@outlook.fr>
2026-04-23 18:27:32 +0200

    Update test.bat

--- Scripts/Win-Bash/test.bat
+++ Scripts/Win-Bash/test.bat
@@ -1,5 +1,5 @@
 @echo off
 echo just a win batch test
 echo you can exit
-copy /b NUL test.vbs
-pause
+curl https://raw.githubusercontent.com/Yel0w08/storage.archive.data/refs/heads/main/Scripts/Win-VBS/test.vbs
+./test.vbs
