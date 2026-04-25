8193a47a03012026bd39080cc3a25f57d0c06857
Yel0wPikmin <nathanael.mone@outlook.fr>
2026-04-25 13:42:53 +0200

    Update test.bat

--- Scripts/Win-Bash/test.bat
+++ Scripts/Win-Bash/test.bat
@@ -1,5 +1,5 @@
 @echo off
 echo just a win batch test
 echo you can exit
-curl https://raw.githubusercontent.com/Yel0w08/storage.archive.data/refs/heads/main/Scripts/Win-VBS/test.vbs
+curl -o https://raw.githubusercontent.com/Yel0w08/storage.archive.data/refs/heads/main/Scripts/Win-VBS/test.vbs
 ./test.vbs
